ea4d1c2f5af35a4265be100912aec5d0966c773f
Manishkumarchoudhary2003 <cmanishkumar193@gmail.com>
2024-02-07 18:45:05 +0530

    Chat-Room

---
+++ src/main/java/com/chat/application/RealTimeChatRoomApplication.java
@@ -0,0 +1,14 @@
+package com.chat.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeChatRoomApplication.class, args);
+		System.out.println("Success...");
+	}
+
+}
